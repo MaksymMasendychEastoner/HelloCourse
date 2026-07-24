a7adda7c30453bcceb7bc0118f5deee4650e7122
Maksym Masendych <maksym.masendych@example.com>
2026-07-24 13:22:21 +0300

    feat: add student LINQ query

---
+++ group_tasks/Block3_Linq/StudentFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace group_tasks.Block3_Linq
+{
+    public record Student(string Name, int Grade);
+
+    public class StudentFilter
+    {
+        public static List<string> GetTopStudentNames(List<Student> students)
+        {
+            // Один LINQ-запит для фільтрації, сортування та вибірки імен
+            var result = students
+                .Where(s => s.Grade >= 60)
+                .OrderByDescending(s => s.Grade)
+                .Select(s => s.Name)
+                .ToList();
+
+            return result;
+        }
+    }
+}
