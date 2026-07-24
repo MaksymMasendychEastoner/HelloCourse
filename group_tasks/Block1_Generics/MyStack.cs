deb5735328f7cee51159ae5b78a61d1b188f49b1
Maksym Masendych <maksym.masendych@example.com>
2026-07-24 13:07:25 +0300

    feat: implement generic MyStack (Block 1 Level 1)

---
+++ group_tasks/Block1_Generics/MyStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace group_tasks.Block1_Generics
+{
+    public class MyStack<T>
+    {
+        // Внутрішній список для збереження елементів стеку
+        private readonly List<T> _items = new List<T>();
+                
+        public void Push(T item)
+        {
+            _items.Add(item);
+        }
+                
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Стек порожній!");
+            }
+                        
+            T lastItem = _items[_items.Count - 1];
+
+            _items.RemoveAt(_items.Count - 1);
+
+            return lastItem;
+        }
+                
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Стек порожній!");
+            }
+
+            return _items[_items.Count - 1];
+        }
+                
+        public bool IsEmpty()
+        {
+            return _items.Count == 0;
+        }
+    }
+}
---
+++ group_tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿using group_tasks.Block1_Generics;
+
+var numbersStack = new MyStack<int>();
+
+numbersStack.Push(10);
+numbersStack.Push(20);
+
+Console.WriteLine($"Верхній елемент (Peek): {numbersStack.Peek()}"); // 20
+Console.WriteLine($"Забрали елемент (Pop): {numbersStack.Pop()}");   // 20
+Console.WriteLine($"Чи порожній стек? {numbersStack.IsEmpty()}");     // False
