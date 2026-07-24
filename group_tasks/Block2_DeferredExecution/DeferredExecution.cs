113f1f09c7c8efc11bc3c3a9bed02727f7c1c491
Maksym Masendych <maksym.masendych@example.com>
2026-07-24 13:17:41 +0300

    feat: implement Deferred Execution (Block 2 Level 1)

---
+++ group_tasks/Block2_DeferredExecution/DeferredExecution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace group_tasks.Block2_DeferredExecution
+{
+    public class DeferredExecutionNotes
+    {
+        public static void RunDemo()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            // LINQ-запит створюється тут, але НЕ виконується
+            var evens = numbers.Where(n => n % 2 == 0);
+                        
+            numbers.Add(6);
+
+            // Запит виконується тут
+            foreach (var n in evens)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
